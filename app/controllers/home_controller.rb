f505aec55457400c65823de0b6518a1476750d89
Rahul Patil <rahulpatil_scs@yahoo.co.in>
2014-05-11 22:57:07 +0530

    BridgeTechSolution:FirstCommit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,19 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def about_us
+  end
+
+  def contact_us
+  end
+
+  def testimonial
+  end
+
+  def post_resume
+  end
+
+  def featured_jobs
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bridgetechsolution::Application.config.secret_token = 'f966ac1f0a27556602995152ae380b9960f0ce4f9170616a1ab4b1ceafc53fbacc837100bf409093ae5b365301e049964719993ff9ec0f7e6e710a15bb0443dc'
